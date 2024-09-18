dc4b8772da56e3d218d9ef545309be43f1c243b1
qcserestipy <patrickeschenbach96@gmail.com>
2024-09-18 11:26:55 +0200

    First commit for repo

---
+++ __main__.py
@@ -0,0 +1,31 @@
+import pulumi
+from network import NetworkStack
+from cluster import GkeClusterStack
+from pulumi_gcp import container
+
+# Load configuration
+config = pulumi.Config()
+
+# Define variables
+cluster_name = config.require("cluster_name")
+region = "europe-west1"
+zone = "europe-west1"
+latest_engine_version = container.get_engine_versions(location=region).release_channel_latest_version['REGULAR']
+pulumi.export("latest_engine_version", latest_engine_version)
+
+# Create the network stack
+network_stack = NetworkStack(
+    name=cluster_name,
+    config=config,
+    region=region
+)
+
+gke_cluster_stack = GkeClusterStack(
+    name=cluster_name,
+    region=region,
+    zone=zone,
+    vpc_id=network_stack.vpc.id,
+    public_subnet_id=network_stack.public_subnet.id,
+    private_subnet_id=network_stack.private_subnet.id,
+    gke_version=latest_engine_version
+)
---
+++ network.py
@@ -0,0 +1,33 @@
+import pulumi
+from pulumi_gcp import compute
+
+class NetworkStack:
+    def __init__(self, name: str, config: pulumi.Config, region: str):
+        # Create a VPC Network
+        self.vpc = compute.Network(
+            f"{name}-vpc",
+            auto_create_subnetworks=False,
+        )
+
+        # Create public and private subnets without the 'tags' argument
+        self.public_subnet = compute.Subnetwork(
+            f"{name}-public-subnet",
+            ip_cidr_range="10.0.0.0/19",
+            region=region,
+            network=self.vpc.id,
+            description="Public Subnet for GKE",
+        )
+
+        self.private_subnet = compute.Subnetwork(
+            f"{name}-private-subnet",
+            ip_cidr_range="10.0.32.0/19",
+            region=region,
+            network=self.vpc.id,
+            description="Private Subnet for GKE",
+            private_ip_google_access=True,
+        )
+
+        # Export the VPC and subnet IDs
+        pulumi.export("vpcId", self.vpc.id)
+        pulumi.export("publicSubnetId", self.public_subnet.id)
+        pulumi.export("privateSubnetId", self.private_subnet.id)
