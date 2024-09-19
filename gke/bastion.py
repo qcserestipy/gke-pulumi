f95bc5745369050257da41c0589a2644aa30667f
qcserestipy <patrickeschenbach96@gmail.com>
2024-09-19 14:55:41 +0200

    Added permissions to bastion host

--- gke/bastion.py
+++ gke/bastion.py
@@ -65,6 +65,26 @@ class GkeBastionHostStack:
                 # Install necessary packages including Tinyproxy
                 sudo apt-get update
                 sudo apt-get install -y google-cloud-sdk tinyproxy
+                sudo apt-get install kubectl google-cloud-cli-gke-gcloud-auth-plugin
+                gcloud container clusters get-credentials yukaringermany-gke \
+                    --region europe-west10 \
+                    --project "yukaringermany-gke"
+                (
+                  set -x; cd "$(mktemp -d)" &&
+                  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+                  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+                  KREW="krew-${OS}_${ARCH}" &&
+                  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+                  tar zxvf "${KREW}.tar.gz" &&
+                  ./"${KREW}" install krew
+                )
+                kubectl krew install ns ctx
+                echo 'export PATH=\"${KREW_ROOT:-$HOME/.krew}/bin:$PATH\"' >> ~/.bashrc
+                echo 'alias kns=\"kubectl ns\"' >> ~/.bashrc
+                echo 'alias kctx=\"kubectl ctx\"' >> ~/.bashrc
+                echo 'alias k=\"kubectl\"' >> ~/.bashrc
+                echo 'source <(kubectl completion bash)' >> ~/.bashrc
+                echo 'complete -F __start_kubectl k' >> ~/.bashrc
 
                 # Configure Tinyproxy
                 sudo sed -i 's/^Port .*/Port 8888/' /etc/tinyproxy/tinyproxy.conf  # Ensure port 8888
