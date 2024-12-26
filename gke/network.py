d1a1097d46d1e26a8f6fedda5f3c9874b8c6ff84
Patrick Eschenbach <patrickeschenbach96@gmail.com>
2024-12-26 22:59:33 +0100

    Added private service network for sql db
    Signed-off-by: Patrick Eschenbach <patrickeschenbach96@gmail.com>

--- gke/network.py
+++ gke/network.py
@@ -1,5 +1,6 @@
 import pulumi
 from pulumi_gcp import compute
+import pulumi_gcp as gcp
 
 class NetworkStack:
     def __init__(self, name: str, region: str):
@@ -95,6 +96,30 @@ class NetworkStack:
             source_ranges=["0.0.0.0/0"],
             description="Allow GitHub Actions runners to access GKE API endpoint",
             priority=1000,
+        )
+
+        # ---------------------------------------------------------------------------------------
+        # Private Service Networking
+        # ---------------------------------------------------------------------------------------
+        #
+        # 1) Reserve an IP range for Google-managed services (e.g. 10.3.0.0/20).
+        # 2) Create a service networking Connection to let private IP GCP services 
+        #    (like Cloud SQL) allocate IP addresses from your VPC.
+
+        self.google_managed_range = compute.GlobalAddress(
+            f"{name}-google-managed-services-range",
+            purpose="VPC_PEERING",
+            address_type="INTERNAL",
+            prefix_length=20,
+            network=self.vpc.self_link,  # needs the self_link, not just .id
+            project=gcp.config.project,
+        )
+
+        self.vpc_peering = gcp.servicenetworking.Connection(
+            f"{name}-vpc-peering",
+            network=self.vpc.self_link,  # again, the full VPC self_link
+            service="servicenetworking.googleapis.com",
+            reserved_peering_ranges=[self.google_managed_range.name],
         )
 
         # Export the VPC and subnet IDs for use in other stacks
